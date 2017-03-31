c3ccc2602218fb3e08932f56254a4bb637356255
matinazari <matinazari.1381@gmail.com>
2017-03-31 22:44:01 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,7 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define(357352395:AAE-
+       whURFvaXotyUw7RdwluqbEJKAS3JuXY, 357352395:AAE-
+      whURFvaXotyUw7Rdw1uqbEJKAS3JuXY);
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
@@ -115,7 +117,7 @@ function processMessage($message) {
   // process incoming message
   $boolean = file_get_contents('booleans.txt');
   $booleans= explode("\n",$boolean);
-  $admin = **ADMIN**;
+  $admin = @matin_azari;
   $message_id = $message['message_id'];
   $rpto = $message['reply_to_message']['forward_from']['id'];
   $chat_id = $message['chat']['id'];
